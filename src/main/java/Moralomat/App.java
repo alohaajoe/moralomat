67d5997a3f0ebd4577a4c2a1f1a21386475bcd81
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2022-10-15 22:07:00 +0200

    Datei Pfad angepasst

--- src/main/java/Moralomat/App.java
+++ src/main/java/Moralomat/App.java
@@ -4,9 +4,9 @@ import Moralomat.Parts.RandomPartsService;
 
 
 public class App {
-    private static final String filePathFirstPart = "src/main/java/Parts/textdata/firstPart.txt";
-    private static final String filePathMiddlePart = "src/main/java/Parts/textdata/middlePart.txt";
-    private static final String filePathLastPart = "src/main/java/Parts/textdata/lastPart.txt";
+    private static final String filePathFirstPart = "src/main/java/Moralomat/Parts/textdata/firstPart.txt";
+    private static final String filePathMiddlePart = "src/main/java/Moralomat/Parts/textdata/middlePart.txt";
+    private static final String filePathLastPart = "src/main/java/Moralomat/Parts/textdata/lastPart.txt";
 
 
     public static void main(String[] args) {
