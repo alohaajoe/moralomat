9f122e16b519fb4d8ab9a33e982adf0ec614139a
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2022-10-16 11:36:52 +0200

    Anscheinend muss eine JSON File ein array bzw. eine List sein. getItAsJSONList Methode in RandomPartsService hinzugefügt. Wenn ich auf der sketch.js den Button drücke, erscheint der Satz nun im Terminal auf Chrome.

--- src/main/java/Moralomat/Parts/RandomPartsService.java
+++ src/main/java/Moralomat/Parts/RandomPartsService.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -22,12 +23,18 @@ public class RandomPartsService {
         lastParts = FileReading.readFile(filePathLastPart);
     }
 
-    @GetMapping("/moral")
     public static String getRandomSentence(){
         String firstPart = getRandomLineFromList(firstParts);
         String middlePart = getRandomLineFromList(middleParts);
         String lastPart = getRandomLineFromList(lastParts);
         return firstPart + middlePart + lastPart;
+    }
+    @GetMapping("/moral")
+    private static List<String> getItAsAJSONList(){
+        String moral = getRandomSentence();
+        List<String> moralJSON = new ArrayList<>();
+        moralJSON.add(moral);
+        return moralJSON;
     }
 
     public static String getRandomLineFromList (List<String> listToGetRandomLineFrom){
